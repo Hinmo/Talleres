905a671f2a5956781719a7457a0fa963baf95dfa
Mario Jurado <jorjuroba@gmail.com>
2024-02-08 20:46:37 -0500

    test

---
+++ Taller2.ts
@@ -0,0 +1,7 @@
+/* Declaren variables utilizando diferentes tipos de datos: número, cadena, booleano,
+arreglo, objeto, etc. */
+
+let nombre: string = "Mario";               //variable cadena
+let mayorEdad: boolean = true;              //variable boolean
+let edad: number = 36;                      //variable number
+let arrayTaller: number[] = [1,2,3,4]       //variable arreglo
